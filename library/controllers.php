6f7a7bec37e30036daf1b10a308419fa132b13d1
brlnstyn <berlianasetyani261@gmail.com>
2022-05-19 21:19:14 +0700

    'Perubahan controllers'

--- library/controllers.php
+++ library/controllers.php
@@ -1,21 +1,24 @@
 <?php
-class oop {
+class oop
+{
 
-    function simpan($koneksi, $table, array $field, $redirect) {
+    function simpan($koneksi, $table, array $field, $redirect)
+    {
         $sql = "INSERT INTO $table SET ";
         foreach ($field as $key => $value) {
-            $sql.=" $key = '$value',";
+            $sql .= " $key = '$value',";
         }
         $sql = rtrim($sql, ',');
         $query = mysqli_query($koneksi, $sql);
         if ($query) {
             echo "<script>alert('Success');document.location.href='$redirect'</script>";
         } else {
-            echo mysqli_error();
+            echo "<script>alert('Gagal');document.location.href='$redirect'</script>";
         }
     }
 
-    function tampil($koneksi, $table) {
+    function tampil($koneksi, $table)
+    {
         $sql = "SELECT * FROM $table";
         $query = mysqli_query($koneksi, $sql);
         while ($data = mysqli_fetch_array($query))
@@ -23,45 +26,50 @@ class oop {
         return $isi;
     }
 
-    function hapus($koneksi, $table, $where, $redirect) {
+    function hapus($koneksi, $table, $where, $redirect)
+    {
         $sql = "DELETE FROM $table WHERE $where";
         $query = mysqli_query($koneksi, $sql);
         if ($query) {
             echo "<script>alert('Success');document.location.href='$redirect'</script>";
         } else {
-            echo mysqli_error();
+            echo "<script>alert('Gagal');document.location.href='$redirect'</script>";
         }
     }
 
-    function edit($koneksi, $table, $where) {
+    function edit($koneksi, $table, $where)
+    {
         $sql = "SELECT * FROM $table WHERE $where";
         $query = mysqli_query($koneksi, $sql);
         $data = mysqli_fetch_array($query);
         return $data;
     }
 
-    function ubah($koneksi, $table, array $field, $where, $redirect) {
+    function ubah($koneksi, $table, array $field, $where, $redirect)
+    {
         $sql = "UPDATE $table SET";
         foreach ($field as $key => $value) {
-            $sql.=" $key = '$value',";
+            $sql .= " $key = '$value',";
         }
         $sql = rtrim($sql, ',');
-        $sql.="WHERE $where";
+        $sql .= "WHERE $where";
         $query = mysqli_query($koneksi, $sql);
         if ($query) {
             echo "<script>alert('Success');document.location.href='$redirect'</script>";
         } else {
-             echo mysqli_error();
+            echo "<script>alert('Gagal');document.location.href='$redirect'</script>";
         }
     }
 
-    function upload($foto, $folder) {
+    function upload($foto, $folder)
+    {
         $tmp = $foto['tmp_name'];
         $namafile = $foto['name'];
         move_uploaded_file($tmp, "$folder/$namafile");
         return $namafile;
     }
-    function login($koneksi, $table, $username, $password, $redirect) {
+    function login($koneksi, $table, $username, $password, $redirect)
+    {
         session_start();
         $username = $_POST['user'];
         $password = $_POST['pass'];
@@ -77,4 +85,3 @@ class oop {
         }
     }
 }
-?>
